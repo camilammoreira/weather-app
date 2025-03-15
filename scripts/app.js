fc18d31e00d1f1460ed266ff8cf7ae0b54d06880
camila.moreira <camilammoreira1@gmail.com>
2025-03-15 14:18:07 -0300

    update UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 // DOM manipulation
 
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
 
 const updateCity = async (city) => {
 
@@ -9,11 +11,25 @@ const updateCity = async (city) => {
     // Get weather with the city details
     const weather = await getWeather(cityDets);
 
+    console.log(cityDets, weather);
     // Return promise with object data
-    return {
-        cityDets: cityDets,
-        weather: weather
-    };
+    return { cityDets, weather };
+}
+
+// Update UI 
+const updateUI = (data) => {
+    // Destructure properties
+    const { cityDets, weather } = data;
+
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.LocalizedName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}&deg;C</span>
+    </div>
+    `;
+
+    card.classList.remove("d-none");
 }
 
 cityForm.addEventListener("submit", e => {
@@ -25,6 +41,6 @@ cityForm.addEventListener("submit", e => {
 
     // Update the UI with new city
     updateCity(city)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(err => console.log(err));
-})
+});
